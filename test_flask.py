3fff9f8bcd6a048562d482cfaf3cffff00fc90ab
gerrard_pan <gerrard_pan@yahoo.com>
2016-07-07 09:57:47 +0800

    add info about rh

--- test_flask.py
+++ test_flask.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return '<h1 style="text-align:center">hello,bitch</h1>'
+    ua = request.headers
+    return '<h1 style="text-align:center">hello,bitch</h1><p>%s</p>' % ua
 @app.route('/<name>')
 def users(name):
     return '<h1 style="text-align:center">hello,%s</h1>' % name
