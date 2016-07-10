6b5e1e780b40b5f0549b910225f2b3b21bc4794b
gerrard_pan <gerrard_pan@yahoo.com>
2016-07-10 16:02:30 +0800

    get return string from server

--- test_flask.py
+++ test_flask.py
@@ -1,12 +1,32 @@
-from flask import Flask,request
+from flask import Flask, request, render_template
+from flask.ext.wtf import Form
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+isreturn = 0
+
 @app.route('/')
 def home():
-    ua = request.headers
-    return '<h1 style="text-align:center">hello,bitch</h1><p>%s</p>' % ua
+    if(isreturn==0):
+        ua = request.headers
+        return render_template('personal.html')
+    pass
+
+@app.route('/artlist/<type>')
+def users(type):
+    if(isreturn==0):
+        ty = type
+        if(ty == 'all'):
+            s = '''<li><a href='/'>aaaaaaaaaaaaaaaaaaa</a></li>
+             <li><a href='/'>bbbbbbbbbbbbbbbbbbb</a></li>
+             <li><a href='/'>ccccccccccccccccccc</a></li>'''
+        return s
+
 @app.route('/<name>')
-def users(name):
-    return '<h1 style="text-align:center">hello,%s</h1>' % name
+def nam(name):
+    if(isreturn==0):
+        ua = request.headers
+        return render_template('personal.html', name=name)
+
 if __name__ == '__main__':
     app.run()
