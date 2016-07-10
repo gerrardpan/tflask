23cf94b97ee96b72736c8da7d963f0c179bdaa79
gerrard_pan <gerrard_pan@yahoo.com>
2016-07-10 17:15:04 +0800

    add db

--- test_flask.py
+++ test_flask.py
@@ -1,9 +1,28 @@
 from flask import Flask, request, render_template
+import sqlite3,os
 from flask.ext.wtf import Form
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 isreturn = 0
+def tdb():
+        conn = sqlite3.connect('test.db')
+        cursor = conn.cursor()
+        #cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+        #cursor.execute('insert into user (id, name) values (\'1\', \'Michael\')')
+        #cursor.close()
+        #conn.commit()
+        #conn.close()
+        # 关闭Connection:
+        print(cursor.rowcount)
+        #conn = sqlite3.connect('test.db')
+        #cursor = conn.cursor()
+        cursor.execute('select * from user where id=?', ('3',))
+        values = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        print(values)
+        return values
 
 @app.route('/')
 def home():
@@ -16,10 +35,12 @@ def home():
 def users(type):
     if(isreturn==0):
         ty = type
+        us = tdb()
+        #print('sfsfsfsfsafafadsfafsafff'+us+'sfsafdasfasfaf')
         if(ty == 'all'):
-            s = '''<li><a href='/'>aaaaaaaaaaaaaaaaaaa</a></li>
-             <li><a href='/'>bbbbbbbbbbbbbbbbbbb</a></li>
-             <li><a href='/'>ccccccccccccccccccc</a></li>'''
+            s = '''<li><a href='/'>%s</a></li>
+             <li><a href='/'>bbbbbbbbbbbbbbbbb</a></li>
+             <li><a href='/'>ccccccccccccccccc</a></li>''' % ty
         return s
 
 @app.route('/<name>')
@@ -27,6 +48,8 @@ def nam(name):
     if(isreturn==0):
         ua = request.headers
         return render_template('personal.html', name=name)
+
+
 
 if __name__ == '__main__':
     app.run()
